a9c302a35dfe0b3ca94ccb48b1eb09b580258e11
Salma Mohamed <101593240+Salma-Moh@users.noreply.github.com>
2024-08-26 13:25:45 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './components/home';
 import GuessNumber from './components/GuessNumber';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
       <Home />
       <GuessNumber />
+      <Analytics />
     </div>
   );
 }
